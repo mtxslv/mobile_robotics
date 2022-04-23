94cd777f018f322e53632a3cc14b4329e4f8450a
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-04-23 16:41:21 -0300

    the test script runs the robot onward and backward and stops it using utils

--- test_1.py
+++ test_1.py
@@ -1,7 +1,7 @@
 # this code aims to control some robot movement.
 
 from utils import *
-
+import sim # remover depois que refatorar
 
 clientID = connect_2_sim()
 test_connection(clientID)
@@ -14,17 +14,6 @@ print(f'return_value_left_motor_control == {return_value_left_motor_control} and
 time.sleep(5)
 return_value_left_motor_control, return_value_right_motor_control = robot_run(clientID, left_motor_handle, right_motor_handle,-1,-1)
 print(f'return_value_left_motor_control == {return_value_left_motor_control} and return_value_right_motor_control == {return_value_right_motor_control}')
-
-                                                           
+time.sleep(5)
+return_value_left_motor_control, return_value_right_motor_control = robot_run(clientID, left_motor_handle, right_motor_handle,0,0)
 print(f'return_value_left_motor_control == {return_value_left_motor_control} and return_value_right_motor_control == {return_value_right_motor_control}')
-"""
-t = time.time()
-current_time = t
-while(current_time-t < 5):
-    current_time = time.time()
-    print(f'it has passed = {current_time-t} secs')
-    return_value_left_motor_control = sim.simxSetJointTargetVelocity(clientID=clientID,jointHandle=left_motor_handle,
-                                                                    targetVelocity=1,operationMode=sim.simx_opmode_streaming)
-    return_value_right_motor_control = sim.simxSetJointTargetVelocity(clientID=clientID,jointHandle=right_motor_handle,
-                                                                    targetVelocity=1,operationMode=sim.simx_opmode_streaming)
-                                                                    """
