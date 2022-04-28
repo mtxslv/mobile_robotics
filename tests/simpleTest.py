0a631269fcc389c9bee3a00fe29199ff3b283812
Jaysa <43121302+JaysaKeylla@users.noreply.github.com>
2022-04-28 16:01:37 -0300

    Update simpleTest.py

--- tests/simpleTest.py
+++ tests/simpleTest.py
@@ -14,7 +14,7 @@ try:
     import os
     import sys
     sys.path.insert(0, os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '../src/')))
+        os.path.join(os.path.dirname(__file__), '../src/') ))
 except:
     print('--------------------------------------------------------------')
     print('"sim.py" could not be imported. This means very probably that')
