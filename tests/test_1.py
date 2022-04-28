a2a518f35217dcc91c932996b0b8893e4377b7f4
Jaysa <43121302+JaysaKeylla@users.noreply.github.com>
2022-04-28 16:00:54 -0300

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -3,7 +3,8 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '../src/')))
+    os.path.join(os.path.dirname(__file__), '../src/') ))
+from utils import *
 
 clientID = connect_2_sim()
 test_connection(clientID)
