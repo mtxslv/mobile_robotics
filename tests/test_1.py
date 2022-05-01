2f0731f07b17f264875a4cb2f096f5df2d4dfa7a
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-05-01 14:59:10 -0300

    the robot now rotate around the center between the wheels at the end of the line movement

--- tests/test_1.py
+++ tests/test_1.py
@@ -26,3 +26,9 @@ return_value_left_motor_control, return_value_right_motor_control = robot_run(
     clientID, left_motor_handle, right_motor_handle, 0, 0)
 print(
     f'return_value_left_motor_control == {return_value_left_motor_control} and return_value_right_motor_control == {return_value_right_motor_control}')
+time.sleep(5)
+return_value_left_motor_control, return_value_right_motor_control = robot_run(
+    clientID, left_motor_handle, right_motor_handle, -1, 1)
+time.sleep(5)
+return_value_left_motor_control, return_value_right_motor_control = robot_run(
+    clientID, left_motor_handle, right_motor_handle, 0, 0)
