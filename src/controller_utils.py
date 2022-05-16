f9c0f42957bebbab42083e1ab27e9b6a221c54f4
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-05-15 21:15:57 -0300

    variable names changed

--- src/controller_utils.py
+++ src/controller_utils.py
@@ -1,8 +1,8 @@
 # this file contains all utilities related to controllers
 import numpy as np
 
-def get_metaparameters(point_1, point_2):
-    vector_difference = point_2 - point_1
+def get_metaparameters(goal_config, initial_config):
+    vector_difference = goal_config - initial_config
     del_L_star_squared = np.power(vector_difference[:2],2)
     del_theta = vector_difference[2]
     del_L_star = np.sqrt(np.sum(del_L_star_squared))
