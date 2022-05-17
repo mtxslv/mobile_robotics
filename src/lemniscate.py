8e5abb360c9349fa04a0300570d0e9cce0a3cde7
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-05-17 13:16:22 -0300

    changed frequency default values and docstring

--- src/lemniscate.py
+++ src/lemniscate.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 
 class Lemniscate:
-    def __init__(self, a_x, a_y, f_x=30, f_y=30):
+    def __init__(self, a_x, a_y, f_x=1/30, f_y=1/30):
         """This class is used to compute a lemniscate curve. It ranges from -a_x to a_x horizontally, 
             and from -a_y to a_y vertically. The movement of a particle over the curve is such that 
             it returns to its original horizontal coordinate in 1/f_x units of time, and to its original
@@ -13,8 +13,8 @@ class Lemniscate:
         Args:
             a_x (float): The X amplitude (how far the curve goes horizontally)
             a_y (float): The Y amplitude (how far the curve goes vertically)
-            f_x (float): The frequency (cicles/second) of the X component. Ideally it equals f_y. (Default:30)
-            f_y (float): The frequency (cicles/second) of the Y component. Ideally it equals f_x. (Default:30)
+            f_x (float): The frequency (cicles/second) of the X component. Ideally it equals f_y. (Default:1/30)
+            f_y (float): The frequency (cicles/second) of the Y component. Ideally it equals f_x. (Default:1/30)
 
         Raises:
             ValueError: raised when a_x is not inside the interval (0 , 2.5]
@@ -49,8 +49,8 @@ class Lemniscate:
             orientation over the lemniscate curve. For position, velocity and acceleration, each array row is a X,Y point.
             For orientation, each row represents the position's orientation (in rad).
         """
-
-        domain = np.linspace(0,1,how_many_points)
+        stop_value = min([1/self.f_x,1/self.f_y])
+        domain = np.linspace(0,stop_value,how_many_points)
 
         lemniscate = np.array( (self.a_x*np.cos(2*np.pi*self.f_x*domain), self.a_y*np.sin(2*2*np.pi*self.f_y*domain)) ).T
         lemniscate_velocity = np.array( (-self.a_x*2*np.pi*self.f_x*np.sin(2*np.pi*self.f_x*domain), 2*self.a_y*2*np.pi*self.f_y*np.cos(2*2*np.pi*self.f_y*domain)) ).T
