5e4d1e400a8daa65b81f8ccdaa71a32e9920adfd
Mateus Assis <mateus.d.assis.silva@gmail.com>
2022-07-02 16:58:36 -0300

    semi-functional sketch

--- tests/simulation_tests/projeto3_meta1.py
+++ tests/simulation_tests/projeto3_meta1.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from pynput import keyboard
+from pynput import keyboard 
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../../src/') ))
@@ -12,29 +12,67 @@ from utils import *
 clientID = connect_2_sim()
 test_connection(clientID)
 
+# Recuperar handlers do dr20
+left_motor_handle, right_motor_handle = get_dr20_motor_handles_(clientID)
+
+# getting sensor handle
+sensor_reference = './dr20/ultrasonicSensorJoint_/ultrasonicSensor_'
+err_code,ultras1 = sim.simxGetObjectHandle(clientID=clientID,
+                                            objectName=sensor_reference,
+                                            operationMode=sim.simx_opmode_blocking)
+
+def move_safe(direction, clientID, left_motor_handle, right_motor_handle):
+    if direction == 'f':
+        print('Moving forward')
+        robot_run(clientID, left_motor_handle, right_motor_handle,1,1)
+    if direction == 'b':
+        print('Moving backward')
+        robot_run(clientID, left_motor_handle, right_motor_handle,-1,-1)
+    if direction == 'l':
+        print('Moving left')
+        robot_run(clientID, left_motor_handle, right_motor_handle,-1,1)
+    if direction == 'r':
+        print('Moving right')
+        robot_run(clientID, left_motor_handle, right_motor_handle,1,-1)
+
+
+# callback function when the key is pressed
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        if key.char == 'a':
+            move_safe('l', clientID, left_motor_handle, right_motor_handle)
+            print('Moving left')
+        if key.char == 'd':
+            move_safe('r', clientID, left_motor_handle, right_motor_handle)
+            print('Moving right')
+        if key.char == 'w':
+            move_safe('f', clientID, left_motor_handle, right_motor_handle)
+            print('Moving forward')
+        if key.char == 's':
+            move_safe('b', clientID, left_motor_handle, right_motor_handle)
+            print('Moving backward')
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
 
+# callback function when the key is released
 def on_release(key):
-    print('{0} released'.format(
-        key))
     if key == keyboard.Key.esc:
+        print(type(key))
         # Stop listener
         return False
-
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
 
 # ...or, in a non-blocking fashion:
 listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
+
+
+while True:
+    detection_state, detection_state_bool, detected_point, distance, normal_surface = sim.simxReadProximitySensor(clientID=clientID, sensorHandle=ultras1, operationMode=sim.simx_opmode_blocking)
+    print(distance)
+    if detection_state_bool:
+        move_safe('r', clientID, left_motor_handle, right_motor_handle)
+        print(f'obstacle found')
+
